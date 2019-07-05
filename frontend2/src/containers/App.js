b193c0a5b1dd9255c0278bb05ce55f59a6861f56
gaulbaram0111 <gaulbaram0111@gmail.com>
2019-07-05 09:52:25 +0900

    190705 9:51

--- frontend2/src/containers/App.js
+++ frontend2/src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Hello from '../../present/Hello.js';
-import MyNavbar from '../../present/nav/MyNavbar.js';
+import Hello from '../components/Hello.jsx/index.js.js.js';
+import MyNavbar from '../components/MyNavbar.jsx/index.js.js.js';
 
 
 class App extends Component{//원래는 이렇게 하면 안됨 App이 루트니까 컴포넌트의 자식이 되면 안됨
