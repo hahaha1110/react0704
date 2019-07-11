4bac10ffc224cecab1d08b71ea48593518c7b0ac
gaulbaram0111 <gaulbaram0111@gmail.com>
2019-07-11 11:25:03 +0900

    0711 11:24

---
+++ frontend4/src/App.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import {Switch, Route, Link, Redirect} from 'react-router-dom'
+import User from './components/User'
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <ul>
+          <li><Link to='/user/apple'>애플</Link></li>
+          <li><Link to='/user/google'>구글</Link></li>
+          <li><Link to='/user/microsoft'>마이크로소프트</Link></li>
+        </ul>
+        <Switch>
+          <Route exact path = '/' render = { () => (<Redirect to = "/user/apple"/>)}/>
+          <Route exact path = '/user/:id' render = { ({match}) => <User user = {match.params.id}/>}/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend4/src/components/User.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function User({user}){
+    return(
+        <div>
+            <h2>User 컴포넌트</h2>
+            <p>User : {user}</p>
+        </div>
+    )
+}
+
+User.propTypes = {
+    user : PropTypes.string
+}
+
+User.defaultProps = {
+    user : 'apple'
+}
